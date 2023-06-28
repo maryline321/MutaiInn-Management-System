91d9a727ef8c21e2fdfd44bd38d764a5e7b0e282
Chebet Maryline <77463649+maryline321@users.noreply.github.com>
2023-06-28 20:45:22 +0300

    depedency updates and Jwt authentication

---
+++ src/main/java/com/inn/cafe/JWT/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.inn.cafe.JWT;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
